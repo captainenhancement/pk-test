fa1d15432b88c9e9359c8861f92cca7964db6235
denisd <captainenhancement@gmail.com>
2019-03-01 22:32:43 +0300

    fix: echoserv indent

--- test/data/echoserv_container5/stalone_echoserv.lua
+++ test/data/echoserv_container5/stalone_echoserv.lua
@@ -4,15 +4,15 @@ socket = require "socket"
 
 local lsock = assert(socket.bind("*", 5000))
 while true do
-	local sock = assert(lsock:accept())
-	local recverr = false
-	while true do
-		local line = sock:receive("*l")
-		if not line then
+  local sock = assert(lsock:accept())
+  local recverr = false
+  while true do
+    local line = sock:receive("*l")
+    if not line then
       sock:close()
-			break
-		end
+      break
+    end
     sock:send(line.."\n")
-	end
+  end
 end
 
