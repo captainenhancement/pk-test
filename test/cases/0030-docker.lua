15c086cf136c3496aa7b9c1e87d27b3de0e06d92
denisd <captainenhancement@gmail.com>
2019-03-04 20:03:20 +0300

    ensure question. state saved. tests fail

--- test/cases/0030-docker.lua
+++ test/cases/0030-docker.lua
@@ -12,10 +12,12 @@ local do_with_docker
         'do_with_docker'
       }
 
-local ensure_equals
+local ensure_equals, ensure_fails_with_substring, ensure_has_substring
       = import 'lua-nucleo/ensure.lua'
       {
-        'ensure_equals'
+        'ensure_equals',
+        'ensure_fails_with_substring',
+        'ensure_has_substring'
       }
 
 --------------------------------------------------------------------------------
@@ -49,8 +51,18 @@ test:case 'run-handler-which-will-crash' (function()
     error('forced error')
   end
 
+  --[[
   local ok = pcall(do_with_docker, CONTAINER_CFG_DIR, handler)
   ensure_equals('handler() crashed (as expected)', ok, false)
+  ]]
+  local test = function()
+    do_with_docker(CONTAINER_CFG_DIR, handler)
+  end
+  ensure_fails_with_substring(
+    'forced handler error test behaved expectedly',
+    test,
+    'forced error'
+  )
 end)
 
 test:case 'run-docker-with-wrong-dir' (function()
@@ -58,8 +70,23 @@ test:case 'run-docker-with-wrong-dir' (function()
     local a = 1 + 2
   end
 
-  local ok = pcall(do_with_docker, WRONG_DIR, handler)
+  local ok, descr = pcall(do_with_docker, WRONG_DIR, handler)
   ensure_equals('setting of wrong dir raises an error (as expected)', ok, false)
+  ensure_has_substring(
+    'wrong dir error has substring',
+    descr,
+    'Can not change dir'
+  )
+  --[[
+  local test = function()
+    do_with_docker(WRONG_DIR, handler)
+  end
+  ensure_fails_with_substring(
+    'forced wrong-dir test behaved unexpectedly',
+    test,
+    'Can not change dir'
+  )
+  ]]
 end)
 
 test:case 'communicate-with-app-inside-contnr-via-tcp' (function()
